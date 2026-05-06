7580de2076ea16950c4b529dd14fb0d90b7fb795
Provash <provash20cb@gmail.com>
2026-05-06 12:06:30 +0600

    Explore conditional type

---
+++ src/m2/conditional2-10.ts
@@ -0,0 +1,20 @@
+//conditional type : depends on condition
+
+type A = null;
+type B = undefined;
+
+// type C = A extends null? true : false;
+
+type C = A extends number? true : B extends undefined? true : false;
+
+type RichPeoplesVehicle ={
+    bike: string;
+    car: string;
+    ship: string;
+};
+
+type CheckVehicle <T>= T extends keyof RichPeoplesVehicle ? true : false;
+
+type HasBike = CheckVehicle<'ship'>
+// type HasBike = CheckVehicle<'tractor'>  //false
+
